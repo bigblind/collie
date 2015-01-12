e70c8a0003ac0881f76ac47f534f0a84831e41bd
Frederik Creemers <frederikcreemers@gmail.com>
2015-01-12 12:11:17 +0100

    Added comments to types and functions.

--- index/doc.go
+++ index/doc.go
@@ -1,18 +1,22 @@
 package index
 
+//Doc represents a document, that can be indexed.
 type Doc struct {
     id string
     data interface{}
 }
 
+//Creates a new Doc with the given id and data.
 func NewDoc(id string, data interface{}) Doc{
     return Doc{id: id, data: data}
 }
 
+//Doc.Id() returns the doc's id.
 func (d *Doc) Id() string{
     return d.id
 }
 
+//doc.Data() returns the doc's data.
 func (d *Doc) Data() interface{} {
     return d.data
 }
--- index/index.go
+++ index/index.go
@@ -1,29 +1,34 @@
 package index
 
+//Index represents a search index, a searchable store of documents.
 type Index struct {
     Name string
     docs map[string]*Doc
 }
 
+//NewIndex creates a new Index with the given name.
 func NewIndex(name string) Index {
     return Index{Name: name, docs: make(map[string]*Doc)}
 }
 
-
+//Index.Put puts a new document in the index, returns an error if the document isn't valid, nil otherwise.
 func (i *Index) Put(d *Doc) error {
     i.docs[d.Id()] = d
     return nil
 }
 
+//Index.Get gets the document with the given id from the index, returns nil if no such doc exists.
 func (i *Index) Get(id string) *Doc {
     return i.docs[id]
 }
 
+//Deletes the document with the given id from the index. If no such doc is found, this function is a noop.
 func (i *Index) Delete(id string) error {
 	delete(i.docs, id)
 	return nil
 }
 
+//Index.Search searches the index using a query string.
 func(i *Index) Searchs(query string) []Doc {
 	r := []Doc{}
 	for _, doc := range i.docs {
